597be08dc462803735f400874b3f7aa727fe87c5
matheodepuydt <150126156+matheodepuydt@users.noreply.github.com>
2024-10-21 09:14:42 +0200

    classe biens
    contient une map de biens, vérifie que l'id est différent quand on ajoute un bien

--- src/Biens.java
+++ src/Biens.java
@@ -1,7 +1,7 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class Biens {
+public class Biens extends IllegalArgumentException {
 	
 	private Map<String,Bien> biens;
 	
@@ -9,11 +9,22 @@ public class Biens {
 		this.biens = new HashMap<String, Bien>();
 	}
 	
-	public void addBien(Bien bien) {
+	public void addBien(Bien bien) throws IllegalArgumentException {
+		for (String key : this.biens.keySet()) {
+			if (key==bien.getId_bien()) {
+				throw new IllegalArgumentException("Cet identifiant est déjà utilisé");
+			}
+		}
 		this.biens.put(bien.getId_bien(), bien);
 	}
 	
 	public Map<String,Bien> getBiens() {
 		return this.biens;
 	}
+	
+	public Bien getBien(String id_bien) {
+		return this.biens.get(id_bien);
+	}
+	
+	
 }
