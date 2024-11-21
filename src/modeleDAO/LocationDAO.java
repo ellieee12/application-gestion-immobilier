05255c4a16d1529cdf070252130110731ee9010f
Ee Ellisa <ellisa.ee@etu.iut-tlse3.fr>
2024-11-21 17:42:00 +0100

    Update LocationDAO.java

--- src/modeleDAO/LocationDAO.java
+++ src/modeleDAO/LocationDAO.java
@@ -17,30 +17,37 @@ private MySQLCon mySQLCon;
 		String id_bien,
 		String id_locataire,
 		Location location) throws SQLException {
-		String reqInsertLocation = "insert into location (id_bien,date_debut,nb_mois,provision_charges_ttc,loyer_ttc,caution_ttc,etat_lieux,"
-				+ "date_derniere_reg,montant_reel_paye,annee,trimestre) "
-				+ "values (?,?,?,?,?,?,?,?,?,?,?)";
-		PreparedStatement stmtInsertLocation = this.mySQLCon.getConnection().prepareStatement(reqInsertLocation);
-		stmtInsertLocation.setString(1, id_bien);
-		stmtInsertLocation.setDate(2, location.getDate_debut());
-		stmtInsertLocation.setInt(3, location.getNb_mois());
-		stmtInsertLocation.setFloat(4, location.getProvision_chargement_TTC());
-		stmtInsertLocation.setFloat(5, location.getLoyer_TTC());
-		stmtInsertLocation.setFloat(6, location.getCaution_TTC());
-		stmtInsertLocation.setString(7,location.getEtat_lieux());
-		stmtInsertLocation.setDate(8, location.getDate_derniere_reg());
-		stmtInsertLocation.setFloat(9, location.getMontant_reel_paye());
-		stmtInsertLocation.setString(10,"2024-01-01");
-		stmtInsertLocation.setInt(11, 1);
-		int i = stmtInsertLocation.executeUpdate();
-		stmtInsertLocation.close();
+		try {
+			String reqInsertLocation = "insert into location (id_bien,date_debut,nb_mois,provision_charges_ttc,loyer_ttc,caution_ttc,etat_lieux,"
+					+ "date_derniere_reg,montant_reel_paye,annee,trimestre) "
+					+ "values (?,?,?,?,?,?,?,?,?,?,?)";
+			PreparedStatement stmtInsertLocation = this.mySQLCon.getConnection().prepareStatement(reqInsertLocation);
+			stmtInsertLocation.setString(1, id_bien);
+			stmtInsertLocation.setDate(2, location.getDate_debut());
+			stmtInsertLocation.setInt(3, location.getNb_mois());
+			stmtInsertLocation.setFloat(4, location.getProvision_chargement_TTC());
+			stmtInsertLocation.setFloat(5, location.getLoyer_TTC());
+			stmtInsertLocation.setFloat(6, location.getCaution_TTC());
+			stmtInsertLocation.setString(7,location.getEtat_lieux());
+			stmtInsertLocation.setDate(8, location.getDate_derniere_reg());
+			stmtInsertLocation.setFloat(9, location.getMontant_reel_paye());
+			stmtInsertLocation.setString(10,"2024-01-01");
+			stmtInsertLocation.setInt(11, 1);
+			int i = stmtInsertLocation.executeUpdate();
+			stmtInsertLocation.close();
+			
+			String reqInsertLouer = "insert into louer (id_bien,date_debut,id_locataire) values (?,?,?)";
+			PreparedStatement stmtInsertLouer = this.mySQLCon.getConnection().prepareStatement(reqInsertLouer);
+			stmtInsertLouer.setString(1, id_bien);
+			stmtInsertLouer.setDate(2, location.getDate_debut());
+			stmtInsertLouer.setString(3, id_locataire);
+			i+=stmtInsertLouer.executeUpdate();
+			stmtInsertLouer.close();
+			return i;
+		}catch(Exception e) {
+			System.out.println(e);
+		}
 		
-		String reqInsertLouer = "insert into louer (id_bien,date_debut,id_locataire) values (?,?,?)";
-		PreparedStatement stmtInsertLouer = this.mySQLCon.getConnection().prepareStatement(reqInsertLouer);
-		stmtInsertLouer.setString(1, id_bien);
-		stmtInsertLouer.setDate(2, location.getDate_debut());
-		stmtInsertLouer.setString(3, id_locataire);
-		return i;
 	}
 	
 	public boolean locationExists(String id_bien, String id_locataire,Date date_debut) {
