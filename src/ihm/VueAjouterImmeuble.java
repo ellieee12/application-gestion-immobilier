b7ac45fadfde541bd6fcdb8bce43933862306dde
Spleedz <maxence.beth@etu.iut-tlse3.fr>
2024-11-21 17:37:12 +0100

    Modifs VueSaisieLocataire et log in sign up

--- src/ihm/VueAjouterImmeuble.java
+++ src/ihm/VueAjouterImmeuble.java
@@ -80,7 +80,7 @@ public class VueAjouterImmeuble extends JFrame {
 		ControleurAjouterImmeuble controleur = ControleurAjouterImmeuble.getControleur();
 		controleur.initialiserControleur(this);
 		
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
