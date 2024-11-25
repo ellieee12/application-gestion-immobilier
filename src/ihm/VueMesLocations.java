b80ffb73caf4771736bd402942d4d05b5036ba9d
Spleedz <maxence.beth@etu.iut-tlse3.fr>
2024-11-25 10:53:27 +0100

    VueMesLocations

---
+++ src/ihm/VueMesLocations.java
@@ -0,0 +1,65 @@
+package ihm;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.border.EmptyBorder;
+
+public class VueMesLocations extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTable table;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VueMesLocations frame = new VueMesLocations();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public VueMesLocations() {
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblNewLabel = new JLabel("Mes Locations  ");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 18));
+		contentPane.add(lblNewLabel, BorderLayout.NORTH);
+		
+		JPanel PanelBouton = new JPanel();
+		contentPane.add(PanelBouton, BorderLayout.EAST);
+		PanelBouton.setLayout(new GridLayout(2, 1, 0, 10));
+		
+		JPanel PanelBoutonNouveau = new JPanel();
+		PanelBouton.add(PanelBoutonNouveau);
+		PanelBoutonNouveau.setLayout(new BorderLayout(0, 0));
+		
+		JButton AjouterImmeuble = new JButton("Ajouter");
+		PanelBoutonNouveau.add(AjouterImmeuble, BorderLayout.SOUTH);
+	}
+
+}
