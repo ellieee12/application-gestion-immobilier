4456d351b39d7e54fb94528b2570c0f73f470758
Ee Ellisa <ellisa.ee@etu.iut-tlse3.fr>
2024-12-04 10:46:34 +0100

    added procedures for facture

--- crud2.sql
+++ crud2.sql
@@ -14,15 +14,8 @@ DROP PROCEDURE IF EXISTS getImmeublesDisponibles$$
 DROP PROCEDURE IF EXISTS getCompteByUsernameMdp$$
 DROP PROCEDURE IF EXISTS insertCompte$$
 DROP PROCEDURE IF EXISTS getMdpByUsername$$
-
-create procedure getBienByCompteur(v_id_compteur)
-BEGIN
-    select type_bien from bien where id_bien = (
-        select c.id_bien 
-        from compteur c 
-        where v_id_compteur=id_compteur
-    );
-END$$
+DROP PROCEDURE IF EXISTS getAllFacture$$
+DROP PROCEDURE IF EXISTS deleteFacture$$
 
 CREATE PROCEDURE getAllBiens()
 BEGIN
@@ -151,7 +144,33 @@ BEGIN
     SELECT mdp from signup where username = v_username;
 END$$
 
+CREATE PROCEDURE getAllFacture()
 
+BEGIN
+    select * from facture;
+END$$
+
+CREATE PROCEDURE deleteFacture(v_numero_facture varchar(50))
+
+BEGIN
+    delete from facture where numero_facture=v_numero_facture;
+END$$
+
+CREATE PROCEDURE insertFacture (
+    v_numero_facture VARCHAR(50),
+    v_date_paiement DECIMAL(15,2),
+    v_date_emission DECIMAL(15,2),
+    v_numero_devis VARCHAR(50),
+    v_designation VARCHAR(50),
+    v_montant_reel_paye DECIMAL(15,2),
+    v_montant DECIMAL(15,2),
+    v_imputable_locataire DECIMAL(15,2),
+    v_id_bien VARCHAR(20)
+)
+BEGIN
+    insert into facture (numero_facture,date_paiement,date_emission,numero_devis,designation,montant_reel_paye,montant,imputable_locataire,id_bien)
+    values (v_numero_facture,v_date_paiement,v_date_emission,v_numero_devis,v_designation,v_montant_reel_paye,v_montant,v_imputable_locataire,v_id_bien);
+END$$
 -- verifier from compteur if bien is a maison or what
 -- insert compteur and releve
 
