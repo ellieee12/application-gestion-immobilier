8965b1f47f6272842e810bb7510ff211d9424322
Gallardo Tristan <tristan.gallardo@etu.iut-tlse3.fr>
2024-12-11 12:24:20 +0100

    new function to change with one int the size of everything

--- src/ihm/VueLogin.java
+++ src/ihm/VueLogin.java
@@ -5,20 +5,34 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.plaf.FontUIResource;
+
 import java.awt.BorderLayout;
 import java.awt.Cursor;
 
 import javax.swing.JLabel;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.Rectangle;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.List;
+
 import javax.swing.JTextField;
 import javax.swing.JPasswordField;
 import javax.swing.JButton;
+import javax.swing.JComponent;
+
 import java.awt.Dimension;
 import javax.swing.SwingConstants;
+import javax.swing.UIManager;
+
 import java.awt.FlowLayout;
 import javax.swing.JSeparator;
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.Container;
 
 public class VueLogin extends JFrame {
 
@@ -31,6 +45,8 @@ public class VueLogin extends JFrame {
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
+		
+		
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -41,6 +57,31 @@ public class VueLogin extends JFrame {
 				}
 			}
 		});
+	}
+	
+	public static List<Component> getAllComponents(final Container c) {
+	    Component[] comps = c.getComponents();
+	    List<Component> compList = new ArrayList<Component>();
+	    for (Component comp : comps) {
+	        compList.add(comp);
+	        if (comp instanceof Container)
+	            compList.addAll(getAllComponents((Container) comp));
+	    }
+	    return compList;
+	}
+	
+	public static void setSizeMulti(List<Component> comps, int i) {
+	    for (Component c : comps) {
+	        if (c.getFont() != null) {
+	        	c.setFont(c.getFont().deriveFont((float) (c.getFont().getSize() + i)));
+	        }
+	        if (c instanceof JButton) {
+	        	c.setPreferredSize(new Dimension((int) c.getPreferredSize().getWidth() + i*2, (int) c.getPreferredSize().getHeight() + i*2));
+	        }
+	        if (c instanceof Container) {
+	        	c.setBounds(new Rectangle(100,100,(int)c.getBounds().getWidth() + i*500,(int) c.getBounds().getHeight() + i*500));
+	        }
+	    }
 	}
 	
 	public VueLogin() {
@@ -132,7 +173,7 @@ public class VueLogin extends JFrame {
 		lblCreerCompte_2.setFont(new Font("Tahoma", Font.PLAIN, 10));
 		panel.add(lblCreerCompte_2);
 		
-		
+		setSizeMulti(getAllComponents(this), 10);
 	}
 	
 	public JTextField getUsername() {
