a7156aa03965e0fa19795b9725f5c30ebb230b21
Depuydt Matheo <matheo.depuydt@etu.iut-tlse3.fr>
2024-12-18 10:19:21 +0100

    Update crud2.sql

--- crud2.sql
+++ crud2.sql
@@ -285,7 +285,7 @@ BEGIN
     insert into location (id_bien,date_debut,nb_mois,colocation,provision_charges_ttc,
     loyer_ttc,caution_ttc,annee,trimestre)
     values (v_id_bien,v_date_debut,v_nb_mois,v_colocation,
-    v_provision_charges_ttc,v_loyer_ttc,v_caution_ttc)
+    v_provision_charges_ttc,v_loyer_ttc,v_caution_ttc, v_annee, v_trimestre)
 END$$
 
 CREATE PROCEDURE insertCompteur (
