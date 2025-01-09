a196f753a1dc7bac20e3da9fb42e679ab5d4c7f7
matheodepuydt <150126156+matheodepuydt@users.noreply.github.com>
2025-01-09 17:34:33 +0100

    création controleur

---
+++ src/main/controleur/ControleurDeclaration.java
@@ -0,0 +1,18 @@
+package controleur;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import ihm.VueDeclaration;
+
+public class ControleurDeclaration implements ActionListener {
+	
+	private VueDeclaration vue;
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
